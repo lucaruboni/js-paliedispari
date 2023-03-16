dc4dcd53555e0fae1f231bd6b332013b3b93089d
lucaruboni <lucaruboni@gmail.com>
2023-03-16 16:06:50 +0100

    [add] now the cicle return a word not undefined+word

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,17 +9,18 @@ L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un n
 //chiedo all'utente la parola e la stampo in console
 
 const word = prompt('scrivi una parola');
-console.log(word)
+console.log(`la tua parola è ${word}`)
 
 //controllo se la/e parola/e inserite sono palindrome con una funzione
 
 // creo il ciclo for che leggerà la parola al contrario
 // i è uguale alla lunghezza della parola inserita - 1 (perchè conta da zero)
 // la condizione è che "i" arrivi alla posizione 0 partendo dll'ultima, ovvero la prima lettera della parola, per farlo uso un decremento 
-let backwords;
+let backwords = '';
 
 for (let i = word.length - 1; i >= 0; i--) {
     backwords += word[i];
-    console.log(backwords)
+    
     
 }
+console.log(backwords)
