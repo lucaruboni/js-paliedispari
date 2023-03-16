258ef2ef86cfe36cee54614e607d9badcf25ab5b
lucaruboni <lucaruboni@gmail.com>
2023-03-16 16:39:20 +0100

    [add] transformed the for cicle in a function

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,16 +11,49 @@ L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un n
 const word = prompt('scrivi una parola');
 console.log(`la tua parola è ${word}`)
 
+
+
+
 //controllo se la/e parola/e inserite sono palindrome con una funzione
+
+
+
+
+// creo una variabile con contenuto vuoto'' in modo che quello che ci verrà inserito risulti una stringa
+
+/* let backwords = ''; */
 
 // creo il ciclo for che leggerà la parola al contrario
 // i è uguale alla lunghezza della parola inserita - 1 (perchè conta da zero)
 // la condizione è che "i" arrivi alla posizione 0 partendo dll'ultima, ovvero la prima lettera della parola, per farlo uso un decremento 
-let backwords = '';
 
-for (let i = word.length - 1; i >= 0; i--) {
-    backwords += word[i];
+/* for (let i = word.length - 1; i >= 0; i--) { */
+
+    //la var backword con contenuto '' verrà riempita con le lettere della parola inserita al contrario fino alla posizione 0 (+= in modo che le lettere si sommino e non si sostituiscano)
+/*     backwords += word[i];
     
     
 }
 console.log(backwords)
+ */
+
+// ora trasporto questo ciclo dentro una funzione
+
+
+
+function palindromeReader(words){
+    let backwords = '';
+
+    for (let i = word.length - 1; i >= 0; i--) {
+
+        backwords += word[i];
+    }
+
+    if (backwords === word) {
+        return true
+    }
+
+    return false
+
+    
+}
